4fed2c3f602a6f324d1cb7d2626b34ab3efa9e9d
Richard Ma <rich.maaa@gmail.com>
2018-08-09 14:59:23 -0700

    Add text and image to splash

--- frontend/components/homepage/splash.jsx
+++ frontend/components/homepage/splash.jsx
@@ -3,12 +3,12 @@ import { Link, Route } from 'react-router-dom';
 
 
 const Splash = () => (
-	<div className="">
+	<div className="splash">
 		<h1>It's time to ditch Skype and TeamSpeak.</h1>
 		<p>All-in-one voice and text chat for gamers thats free, secure, and works on both your desktop and phone.  Stop paying for TeamSpeak servers and hassling with Skype.  Simplfy your life.</p>
 
-		<img src="/assets/images/splashImage.png" alt=""/>
     </div>
+
 );
 
 export default Splash;
