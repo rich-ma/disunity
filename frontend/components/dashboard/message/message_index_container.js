63ae2ce24b2033e15ff676f42f95f6673d717a6d
Richard Ma <rich.maaa@gmail.com>
2018-09-17 17:07:33 -0700

    Test mDTP

--- frontend/components/dashboard/message/message_index_container.js
+++ frontend/components/dashboard/message/message_index_container.js
@@ -1,4 +1,21 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { getMessages } from '../../../reducers/selector';
+import 
 
-const mSTP = 
+
+const mSTP = ( state, ownProps ) => {
+  const channel = state.entities.channels[ownProps.match.params.channelId];
+  const currentUser = state.entities.users[state.session.id];
+  const messages = getMessages(state, parseInt(ownProps.match.params.channelId));
+
+  return ({
+    channel,
+    currentUser,
+    messages,
+    users: state.entities.users,
+    loading: state.ui.loading,
+  })
+};
+
+const mDTP = 
