5ff40285a11b8565726ebb520a11d90e1138bb15
Richard Ma <rich.maaa@gmail.com>
2018-09-21 11:45:54 -0700

    Add http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://disunity.herokuapp.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'https://disunity.herokuapp.com', /http:\/\/disunity.herokuapp.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://disunity.herokuapp.com', 'http://disunity.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
